9c34ecc961f5225efb363c5312983945e38e4389
Anders Kaestner <anders.kaestner@psi.ch>
2021-01-17 19:00:53 +0100

    Remove tnt include

--- applications/NIQualityAssessment/NIQA/niqamainwindow.cpp
+++ applications/NIQualityAssessment/NIQA/niqamainwindow.cpp
@@ -1176,7 +1176,7 @@ void NIQAMainWindow::fitEdgeProfile(arma::vec &dataX, arma::vec &dataY, arma::ve
         int maxpos=0;
         int minpos=0;
         int idx=0;
-        for (auto i=0; i<dataY.n_elem ; ++i)
+        for (size_t i=0; i<dataY.n_elem ; ++i)
         {
             if (maxval<dataY[i])
             {
@@ -1192,7 +1192,7 @@ void NIQAMainWindow::fitEdgeProfile(arma::vec &dataX, arma::vec &dataY, arma::ve
         }
 
         double halfmax=(maxval-minval)/2+minval;
-        int HWHM=maxpos;
+        size_t HWHM=maxpos;
 
         for (; HWHM<dataY.n_elem; ++HWHM)
         {
@@ -1245,7 +1245,7 @@ void NIQAMainWindow::estimateCollimation()
 
     QLineSeries *series = new QLineSeries(); //Life time
 
-    const double eps=0.1;
+    const double eps=0.01;
     for (size_t i=0; i<N; ++i)
     {
         auto item = dynamic_cast<EdgeInfoListItem *>(ui->listWidget_edgeInfo->item(i));
